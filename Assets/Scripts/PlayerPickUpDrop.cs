76829dc3bbc8536ef135ceb25e43c191bd20909f
wangwiza <william.wang4@mail.mcgill.ca>
2024-01-20 13:58:03 -0500

    (feat): scripts for picking up/dropping objects

--- Assets/Scripts/PlayerPickUpDrop.cs
+++ Assets/Scripts/PlayerPickUpDrop.cs
@@ -6,8 +6,13 @@ public class PlayerPickUpDrop : MonoBehaviour
 {
     [SerializeField]
     private LayerMask pickUpLayerMask;
+    [SerializeField]
+    private Transform objectGrabPointTransform;
     private CharacterController controller;
     private bool grabbing;
+
+    private ObjectGrabbable objectGrabbable;
+
 
     // Start is called before the first frame update
     private void Start()
@@ -22,12 +27,23 @@ public class PlayerPickUpDrop : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (grabbing) {
-            float pickUpDistance = 2f;
-            if (Physics.Raycast(transform.position, transform.forward, out RaycastHit raycastHit, pickUpDistance, pickUpLayerMask)) {
-                Debug.Log(raycastHit.transform);
-                Debug.Log("hahaha");
-                
+        if (grabbing)
+        {
+            if (objectGrabbable == null)
+            {
+                // not carrying, try to grab
+                float pickUpDistance = 2f;
+                if (Physics.Raycast(transform.position + new Vector3(0f, -0.5f, 0f), transform.forward, out RaycastHit raycastHit, pickUpDistance, pickUpLayerMask))
+                {
+                    if (raycastHit.transform.TryGetComponent(out objectGrabbable))
+                    {
+                        objectGrabbable.Grab(objectGrabPointTransform);
+                    }
+
+                }
+            } else {
+                objectGrabbable.Drop();
+                objectGrabbable = null;
             }
         }
     }
